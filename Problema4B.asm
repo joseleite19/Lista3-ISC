4ce3b00ea22c4f8de009d658c7c05b26c5a03aa4
luisfbgs <arueluis@gmail.com>
2016-06-05 00:24:39 -0300

    Update Problema4B.asm

--- Problema4B.asm
+++ Problema4B.asm
@@ -1,124 +1,123 @@
 .data
 ENTRADA: .double -7,0,0,2142,5,0,0,0,999999 #f6,f8,f10,f12,f14,f16,f18,f2,f4
 CONSTANTES: .double 0.000000001,0,2,1
-EXPBASE: .double 0,0,0,0	#base,resultado da exponenciaÁ„o
+EXPBASE: .double 0,0,0,0	#base,resultado da exponencia√ß√£o,resultado da soma das exponencia√ß√µes base f2,resultado da soma das exponencia√ß√µes base f22
 SAIDA: .double 2
 .text
-	#f2=a,f4=b,f6=p,f8=q,f10=r,f12=s,f14=i,f16=j,f18=k,f22=x
-	j MAIN			#Chama a funÁ„o MAIN
+	j MAIN			#Chama a fun√ß√£o MAIN
 POW: 	ldc1 $f30,0($s1)	#Carrega em $f30 o valor da base
 	ldc1 $f24,0($s1)	#Carrega em $f24 o valor da base
-	c.eq.d 0,$f30,$f28	#Verifica se a base È zero
-	bc1t FIM0
-	blt $a1,$zero POWN1	#Verifica se o expoente È negativo ou positivo
-	beq $a1,$zero POW0	#Verifica se o expoente È 0
-POWP1:	beq $a1,$zero FIMP1	#Verifica se o expoente È  0
+	c.eq.d 0,$f30,$f28	#Verifica se a base √© zero
+	bc1t FIM0		#Vai para a label FIM0 caso a base seja zero
+	blt $a1,$zero POWN1	#Verifica se o expoente √© negativo ou positivo
+	beq $a1,$zero POW0	#Verifica se o expoente √© 0
+POWP1:	beq $a1,$zero FIMP1	#Verifica se o expoente √©  0
 	mul.d $f30,$f30,$f24	#Multiplica $f30 por $f24 e coloca o resultado em $f30
 	addi $a1,$a1,-1		#Decrementa o valor de $a1(expoente) em 1
-	j POWP1			#Pula para o fim da potenciaÁ„o com expoente positivo
-POWN1:	beq $a1,$zero FIMN1	#Verifica se o expoente È diferente de 0
+	j POWP1			#Pula para o fim da potencia√ß√£o com expoente positivo
+POWN1:	beq $a1,$zero FIMN1	#Verifica se o expoente √© diferente de 0
 	mul.d $f30,$f30,$f24	#Multiplica $f30 por $f24 e coloca o resultado em $f30
 	addi $a1,$a1,1		#Incrementa o valor de $a1(expoente) em 1
-	j POWN1			#Pula para o fim da potenciaÁ„o com expoente negativo
-POW0:	add.d $f30,$f28,$f20
-	sdc1 $f30,8($s1)	
-	jr $ra	
+	j POWN1			#Pula para o fim da potencia√ß√£o com expoente negativo
+POW0:	add.d $f30,$f28,$f20	#Coloca o valor 0 no registrador $f30
+	sdc1 $f30,8($s1)	#Salva o valor de $f30 no endere√ßo $s1+8
+	jr $ra			#Retorna √† fun√ß√£o principal
 FIMN1:	div.d $f30,$f30,$f24	#Divide $f30 por $f24 e coloca o resultado em $f30
 	div.d $f30,$f20,$f30	#Divide 1 por $f30 e coloca o resultado em $f30
-	sdc1 $f30,8($s1)	#Armazena o resultado da potenciaÁ„o no endereÁo $s1+8
-	jr $ra			#Retorna ‡ funÁ„o principal
+	sdc1 $f30,8($s1)	#Armazena o resultado da potencia√ß√£o no endere√ßo $s1+8
+	jr $ra			#Retorna √† fun√ß√£o principal
 FIMP1:	div.d $f30,$f30,$f24	#Divide $f30 por $f24 e coloca o resultado em $f30
-	sdc1 $f30,8($s1)	#Armazena o resultado da potenciaÁ„o no endereÁo $s1+8	
-	jr $ra			#Retorna ‡ funÁ„o principal
-FIM0:	sdc1 $f30,8($s1)
-	jr $ra
-MAIN:	la $s0,ENTRADA 		#Coloca o endereÁo da label ENTRADA no registrador $s0
-	la $s1,EXPBASE 		#Coloca o endereÁo da label ENTRADA no registrador $s0
-	la $s2,SAIDA 		#Coloca o endereÁo da label ENTRADA no registrador $s0
-	la $s3,CONSTANTES	#Coloca o endereÁo da label ENTRADA no registrador $s0
-	ldc1 $f0,0($s3)		#Coloca o valor no endereÁo $s3+0 no registrador $s1
-	ldc1 $f2,56($s0)	#Coloca o valor no endereÁo $s0+56 no registrador $f2
-	ldc1 $f4,64($s0)	#Coloca o valor no endereÁo $s0+64 no registrador $f4
-	ldc1 $f6,0($s0)		#Coloca o valor no endereÁo $s0+0 no registrador $f6	
-	ldc1 $f8,8($s0)		#Coloca o valor no endereÁo $s0+8 no registrador $f8
-	ldc1 $f10,16($s0)	#Coloca o valor no endereÁo $s0+16 no registrador $f10
-	ldc1 $f12,24($s0)	#Coloca o valor no endereÁo $s0+24 no registrador $f12
-	ldc1 $f14,32($s0)	#Coloca o valor no endereÁo $s0+32 no registrador $f14
-	ldc1 $f16,40($s0)	#Coloca o valor no endereÁo $s0+40 no registrador $f16
-	ldc1 $f18,48($s0)	#Coloca o valor no endereÁo $s0+48 no registrador $f16
-	ldc1 $f28,8($s3)	#Coloca o valor no endereÁo $s3+8 no registrador $f28
-	ldc1 $f26,16($s3)	#Coloca o valor no endereÁo $s3+16 no registrador $f26
-	ldc1 $f20,24($s3)	#Coloca o valor no endereÁo $s3+24 no registrador $f20
+	sdc1 $f30,8($s1)	#Armazena o resultado da potencia√ß√£o no endere√ßo $s1+8	
+	jr $ra			#Retorna √† fun√ß√£o principal
+FIM0:	sdc1 $f30,8($s1)	#Salva o valor de $f30 no endere√ßo $s1+8
+	jr $ra			#Retorna √† fun√ß√£o principal
+MAIN:	la $s0,ENTRADA 		#Coloca o endere√ßo da label ENTRADA no registrador $s0
+	la $s1,EXPBASE 		#Coloca o endere√ßo da label ENTRADA no registrador $s0
+	la $s2,SAIDA 		#Coloca o endere√ßo da label ENTRADA no registrador $s0
+	la $s3,CONSTANTES	#Coloca o endere√ßo da label ENTRADA no registrador $s0
+	ldc1 $f0,0($s3)		#Coloca o valor no endere√ßo $s3+0 no registrador $s1
+	ldc1 $f2,56($s0)	#Coloca o valor no endere√ßo $s0+56 no registrador $f2
+	ldc1 $f4,64($s0)	#Coloca o valor no endere√ßo $s0+64 no registrador $f4
+	ldc1 $f6,0($s0)		#Coloca o valor no endere√ßo $s0+0 no registrador $f6	
+	ldc1 $f8,8($s0)		#Coloca o valor no endere√ßo $s0+8 no registrador $f8
+	ldc1 $f10,16($s0)	#Coloca o valor no endere√ßo $s0+16 no registrador $f10
+	ldc1 $f12,24($s0)	#Coloca o valor no endere√ßo $s0+24 no registrador $f12
+	ldc1 $f14,32($s0)	#Coloca o valor no endere√ßo $s0+32 no registrador $f14
+	ldc1 $f16,40($s0)	#Coloca o valor no endere√ßo $s0+40 no registrador $f16
+	ldc1 $f18,48($s0)	#Coloca o valor no endere√ßo $s0+48 no registrador $f16
+	ldc1 $f28,8($s3)	#Coloca o valor no endere√ßo $s3+8 no registrador $f28
+	ldc1 $f26,16($s3)	#Coloca o valor no endere√ßo $s3+16 no registrador $f26
+	ldc1 $f20,24($s3)	#Coloca o valor no endere√ßo $s3+24 no registrador $f20
 LOOP:	add.d $f22,$f2,$f4	#Coloca em $f22 o valor da soma de $f2 com $f4
-	div.d $f22,$f22,$f26	#Coloca em $f22 o valor da divis„o de $f22 por 2
-	cvt.w.d $f14,$f14	#Converte o valor de $f14 para inteiro(word)	#PrÛxima potencia:p*(pow(a,i))
+	div.d $f22,$f22,$f26	#Coloca em $f22 o valor da divis√£o de $f22 por 2
+	cvt.w.d $f14,$f14	#Converte o valor de $f14 para inteiro(word)	#Pr√≥ximo bloco:$f6*(pow($f2,$f14))
 	mfc1 $t0,$f14		#Move o valor de $f14 para $t0
 	add.d $f30,$f2,$f28	#Coloca o valor de $f2 em $f30
-	sdc1 $f2,0($s1)		#Armazena o valor de $f2 no endereÁo $s1+0
+	sdc1 $f2,0($s1)		#Armazena o valor de $f2 no endere√ßo $s1+0
 	addi $a1,$t0,0		#Coloca o valor de $t0 em $a1
-	jal POW			#Chama a funÁ„o de potenciaÁ„o
+	jal POW			#Chama a fun√ß√£o de potencia√ß√£o
 	mul.d $f30,$f30,$f6	#Multiplica $f30 por $f6 e coloca o resultado em $f30
-	sdc1 $f30,16($s1)	#Armazena o resultado de p*(pow(a,i)) no endereÁo $s1+16
-	cvt.w.d $f16,$f16	#Converte o valor de $f14 para inteiro(word)	#PrÛxima potencia:q*(pow(a,j))
+	sdc1 $f30,16($s1)	#Armazena o resultado de p*(pow(a,i)) no endere√ßo $s1+16
+	cvt.w.d $f16,$f16	#Converte o valor de $f14 para inteiro(word)	#Pr√≥ximo bloco:$f8*(pow($f2,$f16))
 	mfc1 $t0,$f16		#Coloca o valor de $f16 em $t0
 	add.d $f30,$f2,$f28	#Coloca o valor de $f2 em $f30
-	sdc1 $f2,0($s1)		#Armazena ov valor de $f2 no endereÁo $s1+0
+	sdc1 $f2,0($s1)		#Armazena ov valor de $f2 no endere√ßo $s1+0
 	addi $a1,$t0,0		#Coloca o valor de $t0 em $a1
-	jal POW			#Chama a funÁ„o POW
+	jal POW			#Chama a fun√ß√£o POW
 	mul.d $f30,$f30,$f8	#Multiplica $f30 por $f8 e coloca o resultado em $f30
-	ldc1 $f24,16($s1)	#Carrega o resultado armazenado no endereÁo $s1+16 em $f24
+	ldc1 $f24,16($s1)	#Carrega o resultado armazenado no endere√ßo $s1+16 em $f24
 	add.d $f30,$f30,$f24	#Soma o valor de $f30 com $f24 e armazena o resultado em $f30
-	sdc1 $f30,16($s1)	#Armazena o resultado de p*(pow(a,i))+q*(pow(a,j)) no endereÁo $s1+16
-	cvt.w.d $f18,$f18	#Converte o valor de $f18 para inteiro(word)		#PrÛxima potencia:r*(pow(a,k))
+	sdc1 $f30,16($s1)	#Armazena o resultado de p*(pow(a,i))+q*(pow(a,j)) no endere√ßo $s1+16
+	cvt.w.d $f18,$f18	#Converte o valor de $f18 para inteiro(word)		#Pr√≥ximo bloco:$f10*(pow($f2,$f18))
 	mfc1 $t0,$f18		#Move o valor de $f18 para $t0
 	add.d $f30,$f2,$f28	#Coloca o valor de $f2 em $f30
-	sdc1 $f2,0($s1)		#Armazena o valor de $f2 no endereÁo $s1+0
+	sdc1 $f2,0($s1)		#Armazena o valor de $f2 no endere√ßo $s1+0
 	addi $a1,$t0,0		#Coloca o valor de $t0 em $a1
-	jal POW			#Chama a funÁ„o POW
+	jal POW			#Chama a fun√ß√£o POW
 	mul.d $f30,$f30,$f10	#Multiplica $f30 por $f10 e coloca o resultado em $f30
-	ldc1 $f24,16($s1)	#Carrega o resultado armazenado no endereÁo $s1+16 em $f24
+	ldc1 $f24,16($s1)	#Carrega o resultado armazenado no endere√ßo $s1+16 em $f24
 	add.d $f30,$f30,$f24	#Soma o valor de $f30 com $f24 e armazena o resultado em $f30
 	add.d $f30,$f30,$f12	#Soma o valor de $f30 com $f12 e armazena o resultado em $f30
-	sdc1 $f30,16($s1)	#Armazena o resultado no endereÁo $s1+16
-	cvt.w.d $f14,$f14	#PrÛxima potencia:p*(pow(x,i))
+	sdc1 $f30,16($s1)	#Armazena o resultado no endere√ßo $s1+16
+	cvt.w.d $f14,$f14	#Pr√≥ximo bloco:$f6*(pow($f22,$f14))
 	mfc1 $t0,$f14
 	add.d $f30,$f22,$f28
 	sdc1 $f22,0($s1)
 	addi $a1,$t0,0
-	jal POW			#Chama a funÁ„o POW
+	jal POW			#Chama a fun√ß√£o POW
 	mul.d $f30,$f30,$f6
 	sdc1 $f30,24($s1)
-	cvt.w.d $f16,$f16	#PrÛxima potencia:q*(pow(x,j))
+	cvt.w.d $f16,$f16	#Pr√≥ximo bloco:$f8*(pow($f22,$f16))
 	mfc1 $t0,$f16
 	add.d $f30,$f22,$f28
 	sdc1 $f22,0($s1)
 	addi $a1,$t0,0
-	jal POW			#Chama a funÁ„o POW
+	jal POW			#Chama a fun√ß√£o POW
 	mul.d $f30,$f30,$f8
 	ldc1 $f24,24($s1)
 	add.d $f30,$f30,$f24	
 	sdc1 $f30,24($s1)
-	cvt.w.d $f18,$f18	#PrÛxima potencia:r*(pow(x,k))
+	cvt.w.d $f18,$f18	#Pr√≥ximo bloco:$f10*(pow($f22,$f18))
 	mfc1 $t0,$f18
 	add.d $f30,$f22,$f28
 	sdc1 $f22,0($s1)
 	addi $a1,$t0,0
-	jal POW			#Chama a funÁ„o POW
+	jal POW			#Chama a fun√ß√£o POW
 	mul.d $f30,$f30,$f10
 	ldc1 $f24,24($s1)
 	add.d $f30,$f30,$f24	
 	add.d $f30,$f30,$f12
 	ldc1 $f24,16($s1)
-	mul.d $f30,$f30,$f24 	#Armazena ((p*pow(a,i)+q*pow(a,j)+r*pow(a,k)+s)*(p*pow(x,i)+q*pow(x,j)+r*pow(x,k)+s)) em $f30
+	mul.d $f30,$f30,$f24 	#Armazena o valor de $f30*$f24 em $f30
 	sdc1 $f30,24($s1)
-	c.lt.d 0,$f30,$f28	#
-	bc1f ELSE
-	add.d $f4,$f22,$f28
-	j FELSE
-ELSE:	add.d $f2,$f22,$f28
-FELSE:	sub.d $f24,$f4,$f2
-	c.lt.d 0,$f0,$f24
-	bc1t  LOOP
-	add.d $f2,$f2,$f4
-	div.d $f2,$f2,$f26
-	sdc1 $f2,0($s2)
+	c.lt.d 0,$f30,$f28	#Caso $f30 seja menor que $f28 coloca o valor 1 na Coditional Flag 0,caso contrario coloca o valor 0 na Coditional Flag 0
+	bc1f ELSE		#Pula para a label ELSE caso a Coditional Flag 0 contenha o valor 0
+	add.d $f4,$f22,$f28	#Coloca o valor de $f22 no registrador $f4
+	j FELSE			#Pula para a label FELSE
+ELSE:	add.d $f2,$f22,$f28	#Coloca o valor de $f22 no registrador $f2
+FELSE:	sub.d $f24,$f4,$f2	#Coloca o valor de $f4 menos $f2 no registrador $f24
+	c.lt.d 0,$f0,$f24	#Caso $f0 seja menor que $f24 coloca o valor 1 na Coditional Flag 0,caso contrario coloca o valor 0 na Coditional Flag 0
+	bc1t  LOOP		#Pula para a label ELSE caso a Coditional Flag 0 contenha o valor 1
+	add.d $f2,$f2,$f4	#Soma $f2 com $f4 e coloca o resultado em $f2
+	div.d $f2,$f2,$f26	#Divide $f2 por 2 e coloca o resultado em $f2
+	sdc1 $f2,0($s2)		#Salva o valor de $f2 no endere√ßo $s2+0(SAIDA)
